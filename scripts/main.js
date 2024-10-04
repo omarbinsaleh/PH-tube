355d8b5c4d96250158e97378e0a9c97ba5b0eb80
omarbinsaleh <omarbinsaleh44@gmail.com>
2024-10-04 17:01:23 +0400

    added the functionality for the category buttons

--- scripts/main.js
+++ scripts/main.js
@@ -18,12 +18,22 @@ const displayCategories = () => {
 
 const showCategories = (data) => {
    const categoryContainer = document.querySelector("#categories");
+
+   // add "all category" button:
+   const btnAll = document.createElement("button");
+   btnAll.dataset.categoryId = "all";
+   btnAll.classList.add("btn", "bg-red-500", "text-white", "hover:bg-red-500", "hover:text-white", "duration-400", "ring-1", "ring-red-500", "ring-offset-1", "btn-category")
+   btnAll.innerText = "All";
+    addClickHandler(btnAll);
+   categoryContainer.appendChild(btnAll);
+
    data.forEach(item => {
-      const {id, category} = item;
+      const {id, category, category_id:categoryId} = item;
 
       // create and show category button
       const button = document.createElement("button");
-      button.classList.add("btn");
+      button.dataset.categoryId = `${categoryId}`;
+      button.classList.add("btn", "hover:bg-red-500", "hover:text-white", "duration-400", "btn-category");
       button.innerText = category;
       addClickHandler(button);
       categoryContainer.appendChild(button);
@@ -32,12 +42,28 @@ const showCategories = (data) => {
 
 const addClickHandler = (button) => {
    button.addEventListener("click", () => {
-      const allButtons = document.querySelectorAll("button");
+      const allButtons = document.querySelectorAll(".btn-category");
       allButtons.forEach(btn => {
-         btn.classList.remove("bg-green-400", "ring-1", "ring-green-400", "ring-offset-1");
+         btn.classList.remove("text-white", "bg-red-500", "ring-1", "ringredn-500", "ring-offset-1");
       })
 
-      button.classList.add("bg-green-400", "ring-1", "ring-green-400", "ring-offset-1");
+      // add styles for the active button:
+      button.classList.add("text-white", "bg-red-500", "ring-1", "ring-red-500", "ring-offset-1");
+
+      // display videos based on category:
+      if (button.dataset.categoryId === "all") {
+         displayVideos();
+      } 
+      else {
+         fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${button.dataset.categoryId}`)
+         .then(response => response.json())
+         .then(data => {
+            console.log(data.category);
+            showVideos(data.category)
+         })
+         .catch(error => console.error(error));
+      }
+      
    })
 };
 
@@ -49,31 +75,65 @@ const displayVideos = () => {
 };
 
 const showVideos = (data) => {
+   // get the videos container:
    const videoContainer = document.getElementById("videos");
    console.log(videoContainer);
+   // clear the contents of the videos container:
+   clearContent(videoContainer);
 
-   data.forEach((video) => {
-      console.log(video);
-      const {thumbnail, title, description} = video;
-      const {profile_picture:profilePicture, profile_name:profileName} = video.authors[0];
-      const {views, posted_date:publishDate} = video.others;
-
-      // create the video card:
-      const card = document.createElement("div");
-      card.classList = "card card-compact bg-base-100 w-full shadow-sm";
-      card.innerHTML = `
-         <figure class="">
-            <img class="w-full aspect-[5/3]"
-            src=${video.thumbnail}
-            alt="thumbnail" />
-         </figure>
-         <div class="card-body">
-            <h2 class="card-title">${title}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+   // create and display videos in the UI;
+   if (data.length >= 1) {
+      data.forEach((video) => {
+         console.log(video);
+         const {thumbnail, title, description} = video;
+         const {profile_picture:profilePicture, profile_name:profileName, verified} = video.authors[0];
+         const {views, posted_date:publishDate} = video.others;
+   
+         // create the video card:
+         const card = document.createElement("div");
+         card.classList = "card card-compact bg-base-100 w-full shadow-md rounded-none";
+         card.innerHTML = `
+            <figure class="rounded-tr-lg rounded-tl-lg">
+               <img class="w-full aspect-[5/3]"
+               src=${video.thumbnail}
+               alt="thumbnail" />
+            </figure>
+            <div class="p-1 py-3 flex gap-3">
+               <div class="pt-1">
+                  <img class="w-10 aspect-square rounded-full object-cover border border-red-500" src=${profilePicture} alt=${profileName}>
+               </div>
+               <div class="">
+                  <h2 class="font-bold text-lg">${title}</h2>
+                  <div class="flex items-center gap-2 text-gray-500">
+                     <p class="">${profileName}</p>
+                     ${verified ? `<img class="w-4" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000"></img>` : ""}
+                  </div>
+                  <p class="text-sm text-gray-500">${views} Views</p>
+               </div>
+            </div>
+         `;
+         videoContainer.appendChild(card);
+      })
+   }
+   else {
+      videoContainer.innerHTML = `
+         <div class="w-full col-span-full pt-20 flex items-center justify-center flex-col gap-5">
+            <img class="w-[140px] mx-auto" src="../assets/icon.png">
+            <div class="text-center">
+               <p>No Videos to Display</p>
+               <p>☹</p>
+            </div>
          </div>
-      `;
-      videoContainer.appendChild(card);
+      `
+   }
+}
 
-   })
+const clearContent = (parentElement) => {
+   let child = parentElement.lastElementChild;
+
+   while(child) {
+      parentElement.removeChild(child);
+      child = parentElement.lastElementChild;
+   }
 }
 
